958ce866f8deba2db4a1ff04f8be6d2e44a865ad
github username <github email address>
2018-04-25 16:28:08 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+ attr_reader :name
+ attr_accessor :
+
+def initialize(name)
+  @name = name
+end
+
+def roster=(roster)
+ @roster = {}
+end
+
+def roster
+  @roster
+end
+
+end
